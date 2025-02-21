ae150074bf3a5d6ef5f73a89fc3aad835087b228
Alessandro Melino <alessandro.melino0@gmail.com>
2025-02-22 00:28:47 +0100

    Aggiornamento sito: descrizione delle modifiche

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
+const toggleButton = document.getElementById("toggleButton");
+const body = document.body;
 document.addEventListener("DOMContentLoaded", function() {
-  const toggleButton = document.getElementById("toggleButton");
   toggleButton.addEventListener("click", function() {
-    document.body.classList.toggle("light-mode");
+    body.classList.toggle("light-mode");
     const images = document.querySelectorAll("img");
     images.forEach(function(img) {
-      if (document.body.classList.contains("light-mode")) {
+      if (body.classList.contains("light-mode")) {
         images.forEach(image => {
-          Image.classList.remove("hidden");
+          Image.style.display = "block";
         });
       } else {
         images.forEach(image  => {
-        Imgage.classList.add("hidden");
-      }
+          Image.style.display = "none";
     });
   });
 });
