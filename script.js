0b020273192a42fb1d99bac43a8ff747a56286cf
Alessandro Melino <alessandro.melino0@gmail.com>
2025-02-21 22:02:33 +0100

    Aggiornamento sito: descrizione delle modifiche

--- script.js
+++ script.js
@@ -1,12 +1,6 @@
-console.log("script.js caricato correttamente");
 document.addEventListener("DOMContentLoaded", function() {
-    var button = document.getElementById("btn");
-    button.style.backgroundColor = "grey";
-    button.addEventListener("click", function() {
-        if (button.style.backgroundColor === "grey"); {
-          button.style.backgroundColor = "#7DF9FF";
-        } else {
-          button.style.backgroundColor = "grey";
-        }
-    });
+  const toggleButton = document.getElementById("toggleButton");
+  toggleButton.addEventListener("click", function() {
+    document.body.classList.toggle("light-mode");
+  });
 });
