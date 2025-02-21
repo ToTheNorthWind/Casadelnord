bde8b10a4af4b638df7df0a39ed7efeaefab109d
Alessandro Melino <alessandro.melino0@gmail.com>
2025-02-22 00:32:34 +0100

    Aggiornamento sito: descrizione delle modifiche

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
 const toggleButton = document.getElementById("toggleButton");
 const body = document.body;
-document.addEventListener("DOMContentLoaded", function() {
   toggleButton.addEventListener("click", function() {
     body.classList.toggle("light-mode");
     const images = document.querySelectorAll("img");
-    images.forEach(function(img) {
       if (body.classList.contains("light-mode")) {
         images.forEach(image => {
           Image.style.display = "block";
