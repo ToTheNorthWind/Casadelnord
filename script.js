f7c88498627b59a656add16d343db0353dba2dc8
Alessandro Melino <alessandro.melino0@gmail.com>
2025-02-25 11:28:39 +0100

    Aggiornamento sito: descrizione delle modifiche

--- script.js
+++ script.js
@@ -2,6 +2,11 @@ const toggleButton = document.getElementById("toggleButton");
 const body = document.body;
 const imageContainer = document.querySelector(".image-container");
 
+function playSound(src) {
+    let audio = new Audio(src);
+    audio.play();
+}
+
 toggleButton.addEventListener("click", function() {
     body.classList.toggle("light-mode");
     imageContainer.classList.toggle("hidden")
